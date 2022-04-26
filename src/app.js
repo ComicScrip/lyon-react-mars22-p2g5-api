14efdc77a7043b4e56779c568e7c15f10130b93f
Claiirou <clairemachet@hotmail.fr>
2022-04-26 16:53:09 +0200

    before refresh vscode

--- src/app.js
+++ src/app.js
@@ -5,18 +5,30 @@ const app = express();
 
 app.use(express.json());
 
-const items = [
-  { id: 1, name: 'item1' },
-  { id: 2, name: 'item2' },
-];
+// const items = [
+//   { id: 1, name: 'item1' },
+//   { id: 2, name: 'item2' },
+// ];
 
-app.get('/things', (req, res) => {
-  console.log('handling GET /things');
-  res.send(items);
-});
+// app.get('/things', (req, res) => {
+//   console.log('handling GET /things');
+//   res.send(items);
+// });
 
 db.connect((err) => {
   if (err) console.error('error connecting to db');
+});
+
+app.get('/jokes/:id', async (req, res) => {
+  try {
+    const [[jokes]] = await db
+      .promise()
+      .query('SELECT * FROM jokes WHERE id = ?', [req.params.id]);
+    res.send(jokes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
 });
 
 module.exports.app = app;
