60fe585f3eadef6b0bcd40bd58b9c2f265711d82
Claiirou <clairemachet@hotmail.fr>
2022-04-26 17:31:36 +0200

    try things on app.get at the end of the day

--- src/app.js
+++ src/app.js
@@ -19,12 +19,23 @@ db.connect((err) => {
   if (err) console.error('error connecting to db');
 });
 
+app.get('/jokes', async (req, res) => {
+  try {
+    const [[jokes]] = await db.promise().query('SELECT * FROM jokes');
+    res.send(jokes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
+});
+
 app.get('/jokes/:id', async (req, res) => {
   try {
     const [[jokes]] = await db
       .promise()
       .query('SELECT * FROM jokes WHERE id = ?', [req.params.id]);
-    res.send(jokes);
+    if (jokes) res.send(jokes);
+    console.log(jokes);
   } catch (err) {
     console.error(err);
     res.status(500).send('something wrong happened');
