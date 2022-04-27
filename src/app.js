2c1d63d2da26eeb4a9e1b0f778c63afa49330dde
Claiirou <clairemachet@hotmail.fr>
2022-04-27 11:41:04 +0200

    fix issue in adding nmp i cors

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const db = require('./db');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-// const items = [
-//   { id: 1, name: 'item1' },
-//   { id: 2, name: 'item2' },
-// ];
-
-// app.get('/things', (req, res) => {
-//   console.log('handling GET /things');
-//   res.send(items);
-// });
 
 db.connect((err) => {
   if (err) console.error('error connecting to db');
