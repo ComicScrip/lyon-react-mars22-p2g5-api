02674a10677dab086f6eab31da8fd7fe038ebd10
Dervieux <dervieux.ben@gmail.com>
2022-05-04 17:33:12 +0200

    Revert "fix conflicts"
    This reverts commit d134e65194ebc381587455c75e69e718678c8348, reversing
    changes made to 2b7233c0a5c47e3cc510b8361caf1eb20c7ebc19.

--- src/environment.js
+++ src/environment.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-function getEnv(variable) {
-  const value = process.env[variable];
+function getEnv(varibale) {
+  const value = process.env[varibale];
   if (typeof value === 'undefined') {
-    console.warn(`Seems like the variable "${variable}" is not set in the environment. 
+    console.warn(`Seems like the variable "${varibale}" is not set in the environment. 
     Did you forget to execute "cp .env.example .env" and adjust variables in the .env file to match your own environment ?`);
   }
   return value;
